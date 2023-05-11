bfabcdecd81836ef676d62a2076886d3533ee090
ntr0008 <NTR@adminisatorsmbp.mynetworksettings.com>
2023-05-10 20:01:31 -0400

    Updated Website

--- Ecom-Project/JSON.js
+++ Ecom-Project/JSON.js
@@ -5,17 +5,20 @@ http.onload = function () {
   if (this.readyState == 4 && this.status == 200) {
     let products = JSON.parse(this.responseText);
     let output = "";
-    for (let item of products) {
+    for (let product of products) {
       output += `
             <div class="product">
-               <p class="title">${item.shipModel}</p>
-               <img src="${item.shipPhoto}" alt="${item.shipDescription}">
-               <p class="description">${item.shipDescription}</p>
+               <p class="title">${product.shipModel}</p>
+               <img src="${product.shipPhoto}" alt="${product.shipDescription}">
+
+               <p class="description">${product.shipDescription}</p>
                <p class="price">
                   <span>$</span>
-                  <span>${item.shipPrice}</span>
+                  <span>${product.shipPrice}</span>
                </p>
-               <p class="cart">Add to cart <i class="bx bx-cart-alt"></i></p>
+               <button type = "button" class = "add-to-cart-btn">
+                <i class = "fas fa-shopping-cart"></i>Add To Cart
+              </button>
             </div>
          `;
     }
